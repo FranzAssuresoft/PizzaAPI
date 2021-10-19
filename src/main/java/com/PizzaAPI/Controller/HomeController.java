e2db6e8de75a00bccb9ce055cee85d9523b6fbf8
gabbylu94 <gabriela.barahona@trainning.assuresoft.com>
2021-10-18 23:02:03 -0400

    Spring

---
+++ src/main/java/com/PizzaAPI/Controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.PizzaAPI.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+	@RestController
+	public class HomeController {
+
+		@GetMapping("/home")
+		public String bienvenida() {
+			return "Welcome";
+		}
+	}
+
+
